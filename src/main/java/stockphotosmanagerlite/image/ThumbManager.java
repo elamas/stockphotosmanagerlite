1f7f7b7d780810eba4c056409dd84f48c8422d74
Quique <elamas@gmail.com>
2020-06-11 13:04:36 +0000

    Implementado ThumbManager

---
+++ src/main/java/stockphotosmanagerlite/image/ThumbManager.java
@@ -0,0 +1,49 @@
+package stockphotosmanagerlite.image;
+
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class ThumbManager {
+
+	public final static void generateThumb(String fileInput, String fileOutput, int maxWidth, int maxHeight) throws Exception {
+		BufferedImage image = ImageIO.read(new File(fileInput));
+		
+		//calculo el factor de multiplicacion a aplicar en funcion de si es vertical u horizontal
+		int width = image.getWidth();
+		int height = image.getHeight();
+		float factor;
+		
+		if (width > height) {
+			//horizontal
+			factor = (float)maxWidth/width;
+		} else {
+			//vertical o cuadrada
+			factor = (float)maxHeight/height;
+		}
+		
+//        Image tmp = image.getScaledInstance((int)(width*factor), (int)(height*factor), Image.SCALE_SMOOTH);
+//        BufferedImage scaled = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+//        Graphics2D g2d = scaled.createGraphics();
+//        g2d.drawImage(tmp, 0, 0, null);
+//        g2d.dispose();
+//        ImageIO.write(scaled, "JPG", new File(fileOutput));
+		
+		boolean preserveAlpha = true;
+	    int imageType = preserveAlpha ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+	    BufferedImage scaledBI = new BufferedImage((int)(width*factor), (int)(height*factor), imageType);
+	    Graphics2D g = scaledBI.createGraphics();
+	    if (preserveAlpha) {
+	            g.setComposite(AlphaComposite.Src);
+	    }
+	    g.drawImage(image, 0, 0, (int)(width*factor), (int)(height*factor), null); 
+	    g.dispose();
+	    ImageIO.write(scaledBI, "JPG", new File(fileOutput));
+	}
+}
---
+++ src/test/java/stockphotosmanagerlite/image/ThumbManagerTest.java
@@ -0,0 +1,21 @@
+package stockphotosmanagerlite.image;
+
+import java.net.URL;
+
+import org.junit.Test;
+
+public class ThumbManagerTest {
+	
+	@Test
+	public void generateThumbTestHorizontal() throws Exception {
+		URL urlImage = Thread.currentThread().getContextClassLoader().getResource("horizontal.jpg");
+		ThumbManager.generateThumb(urlImage.getPath(), "/quique/tmp/horizontal-scaled", 100, 75);
+	}
+
+	@Test
+	public void generateThumbTestVertical() throws Exception {
+		URL urlImage = Thread.currentThread().getContextClassLoader().getResource("vertical.jpg");
+		ThumbManager.generateThumb(urlImage.getPath(), "/quique/tmp/vertical-scaled", 100, 75);
+	}
+
+}
